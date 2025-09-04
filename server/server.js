957a75353ec1de5a387a188ea196479fd297aa2f
ajinkyachirde07 <ajinkyachirde07@gmail.com>
2025-09-04 12:41:07 +0530

    new

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({ 
     origin:"https://snapshop-zeta.vercel.app",  
-    origin:"https://localhost:5173",
+    origin:"http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
