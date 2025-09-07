a18c48c014668da07ba72816027fce1b993dd658
ajinkyachirde07 <ajinkyachirde07@gmail.com>
2025-09-07 21:01:47 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 
 const allowedOrigins = [
-  "http://localhost:5173", // local dev
+   // local dev
   "https://snapshop-zeta.vercel.app" // vercel prod
 ];
 
